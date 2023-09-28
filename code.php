173f7011a945686a9161af12d5f75b06ba720972
Rounak Dhadiwal <irounakdhadiwal999@gmail.com>
2023-09-28 18:02:03 +0530

    Update code.php

--- code.php
+++ code.php
@@ -1,7 +1,7 @@
 <?php
-if ($_SERVER['HTTP_X_FORWARDED_FOR'] === '127.0.0.1') {
+if ($_SERVER['HTTP_X_ORIGINAL_URL'] === '127.0.0.1') {
     echo 'LEET';
 } else {
-    http_response_code(403);
+    echo '403 FORBIDDEN. PAGE CAN ONLY BE ACCESSED FROM INTERNAL NETWORK.';
 }
 ?>
